ed5a1751c2131053cc0f33cd6a1607f621a27087
Maxime <maxime.leviel@efrei.net>
2020-11-21 14:58:30 +0100

    Delete unused api call

--- server/routes/api.js
+++ server/routes/api.js
@@ -15,35 +15,6 @@ client.connect()
 
 module.exports = router
 
-router.post('/maraude', (req, res) =>{
-  const maraude = req.body.maraude
-  req.session.maraude = maraude
-  res.json({message: 'Maraude selectionnée'})
-})
-
-router.get('/maraude', (req, res) =>{
-  if (req.session.maraude === undefined){
-    res.status(404).json({message: 'Pas de maraude sélectionée'})
-  }
-  res.json(req.session.maraude)
-})
-
-router.get('/emailExist', async (req, res) =>{
-  const email = req.body.email
-
-  const sql = "SELECT * FROM users WHERE email=$1"
-  const result = await client.query({
-    text: sql,
-    values: [email] // ici name et description ne sont pas concaténées à notre requête
-  })
-
-  if (result.rowCount == 0){
-    res.json({exist: false})
-  }
-  res.json({exist: true})
-})
-
-
 //Start of admin part
 router.get('/admin/me', async (req, res) => {
   if(req.session.admin === true){
